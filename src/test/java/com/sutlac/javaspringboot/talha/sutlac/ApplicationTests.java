856f1727b5c1213c5e43d9611881694865ada11c
greys26 <talhaerturk@gmail.com>
2020-08-09 12:53:10 -0400

    Initial commit for SpringBootapp

---
+++ src/test/java/com/sutlac/javaspringboot/talha/sutlac/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sutlac.javaspringboot.talha.sutlac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
